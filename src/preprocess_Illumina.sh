899206aed1085568db088395352d7a2a49fdcace
matheuscburger <matheus.cburger@gmail.com>
2016-09-16 22:01:41 -0300

    Script para pre processar dados da Illumina

---
+++ src/preprocess_Illumina.sh
@@ -0,0 +1,5 @@
+
+
+src/remove_outliers_Illumina.sh GSE13052 2>&1 > log/remove_outliers_Illumina_GSE13052.txt
+
+src/remove_outliers_Illumina.sh GSE28405 2>&1 > log/remove_outliers_Illumina_GSE28405.txt
