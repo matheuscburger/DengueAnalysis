2ef36eb36db101ad9d32d642dd9e98c8cdc69a08
Matheus Burger <mburger@localhost.localdomain>
2016-09-17 23:51:07 -0300

    removi parallel pq nao funciona com funcoes

--- src/getData.sh
+++ src/getData.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for cdir in data/geo_raw/author data/geo_raw/supplemental_data data/geo_raw/raw_data data/geo_raw/sample_annotation data/geo_raw/probe_annotation; do
+for cdir in data/raw_data data/geo_raw/author data/geo_raw/supplemental_data data/geo_raw/raw_data data/geo_raw/sample_annotation data/geo_raw/probe_annotation; do
 	echo "Creating $cdir ..."
 	if [ -d $cdir ]; then
 		echo "$cdir already exists !"
@@ -15,9 +15,11 @@ GetGEO() {
 	src/microarrayAnalysis/get_geo.R $1 --author-dir=data/geo_raw/author/ --sup-dir=data/geo_raw/supplemental_data/ --sample-annot-dir=data/geo_raw/sample_annotation/ --probe-annot-dir=data/geo_raw/probe_annotation/ --raw-dir=data/geo_raw/raw_data/ 1>&2 2> log/get_geo_$1.log 
 }
 
-export -f GetGEO
 
-parallel -j 10 GetGEO ::: GSE43777 GSE51808 GSE13052 GSE28405
+GetGEO GSE43777
+GetGEO GSE51808
+GetGEO GSE13052
+GetGEO GSE28405
 
 src/getDataGSE28405.py data/raw_data/GSE28405_detection_pval.tsv data/raw_data/GSE28405_signal.tsv data/raw_data/GSE28405_samplename2gsm.tsv 
 src/getDataGSE13052.py data/raw_data/GSE13052_detection_pval.tsv data/raw_data/GSE13052_signal.tsv data/raw_data/GSE13052_samplename2gsm.tsv
