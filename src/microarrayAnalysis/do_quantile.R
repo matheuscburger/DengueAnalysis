84ed595f0b453fb5355e43f6cfc54b4d83eb99bc
matheuscburger <matheus.cburger@gmail.com>
2016-09-22 13:27:29 -0300

    Mudanca no do_quantile.R para evitar valores NaN quando log2 é calculado

--- src/microarrayAnalysis/do_quantile.R
+++ src/microarrayAnalysis/do_quantile.R
@@ -2,7 +2,7 @@
 
 "Normalize a expression file
 
-Usage: do_quantile.R INPUT OUTPUT  [--annotation-cols=<value>...]
+Usage: do_quantile.R INPUT OUTPUT  [--annotation-cols=<value>...] [--avoid-0] [--offset=<val>]
 
 Input:
   INPUT                      the input file containing expression values with samples in columns and genes/probes in rows
@@ -14,6 +14,8 @@ Options:
   -h --help                  show this help message
   --version                  show program version
   --annotation-cols=<value>   annotation columns [default: ProbeName]
+  --avoid-0                  if this flag is used and data is not log transformed I will sum abs(min) to all values 
+  --offset=<val>             if avoid-0 is TRUE I will add a offset to all values
 
 Authors:
   Matheus Carvalho Burger - burger at usp.br
@@ -36,9 +38,16 @@ is.log <- function(mat, limit=1000){
 	return(!max(mat, na.rm=TRUE) > 1000)
 }
 
-inner_quantile <- function(exp.df, method="quantile", annotation_cols=c("ProbeName")){
+inner_quantile <- function(exp.df, method="quantile", annotation_cols=c("ProbeName"), avoid_0, offset_val){
 	exp.mat <- normalizeBetweenArrays(as.matrix(drop.cols(exp.df, annotation_cols)), method=method)
 	if(!is.log(exp.mat)){
+		message("Data are not in log2 scale, I will apply log2 function.")
+		min_val <- min(exp.mat)
+		if(avoid_0 && min_val < 0){
+			total_offset <- abs(min_val) + offset_val
+			message("I will add ", round(total_offset, digits=2), " to all values before log transform.")
+			exp.mat <- exp.mat + total_offset
+		}
 		exp.mat <- log2(exp.mat)
 	}
 	exp.df <- cbind(exp.df[, annotation_cols], as.data.frame(exp.mat))
@@ -46,9 +55,9 @@ inner_quantile <- function(exp.df, method="quantile", annotation_cols=c("ProbeNa
 	return(exp.df)
 }
 
-do_quantile <- function(input, output, method="quantile", annotation_cols=c("ProbeName"), ...){
+do_quantile <- function(input, output, method="quantile", annotation_cols=c("ProbeName"), avoid_0, offset_val, ...){
 	exp.df <- read.delim(input)
-	exp.df <- inner_quantile(exp.df, method, annotation_cols)
+	exp.df <- inner_quantile(exp.df, method, annotation_cols, avoid_0, offset_val)
 	write.table(exp.df, output, quote=FALSE, sep="\t", row.names=FALSE)
 }
 
@@ -60,6 +69,12 @@ if (!interactive() && !exists('SOURCE')) {
 	arg <- arg[!sapply(arg, is.null)][-(1:2)]  # filter missing, 'help' and 'version'
 	clean <- function(s) gsub('-', '_', gsub('^-+', '', tolower(s)))
 	names(arg) <- clean(names(arg))
+
+	if(! "offset" %in% names(arg)){
+		message("Setting offset to 0.")
+		arg[["offset"]] <- 0
+	}
+	arg[["offset"]] <- as.numeric(arg[["offset"]])
 
 	parameters <- arg
 
