3f09957af0a06972c0a8f1e1d087c6ee16f6ead4
matheuscburger <matheus.cburger@gmail.com>
2016-10-21 22:22:25 -0200

    some modification on DEGs

--- src/DEG.sh
+++ src/DEG.sh
@@ -19,3 +19,6 @@ parallel -j 10 "src/microarrayAnalysis/do_comparisons.R --comp-file {} --norm-fi
 
 src/microarrayAnalysis/join_stats.R results/GPL2700_joined_DEG.tsv --probe-col ProbeName --gene-col Symbol --input results/DEG/GSE13052.tsv --input results/DEG/GSE28405.tsv --cores 6 
 src/microarrayAnalysis/join_stats.R results/GPL570_joined_DEG.tsv --probe-col ProbeName --gene-col Symbol --input results/DEG/GSE43777.tsv --input results/DEG/GSE51808.tsv --cores 6 
+
+# Junta as duas plataformas
+R CMD BATCH src/cutDEGs.R
