de919188e6b19892921b981f3512726b3e3e0d94
matheuscburger <matheus.cburger@gmail.com>
2016-11-08 13:22:19 -0200

    Find connected components and run enrichr
    Former-commit-id: cdf7a73847b247ad3f80670c1c8f6979d05cf9ef

--- src/CEMiTool.sh
+++ src/CEMiTool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for cdir in results/CEMiTool data/processed/CEMiTool_input/annotated data/processed/CEMiTool_input/gene_conversion; do
+for cdir in results/CEMiTool data/processed/CEMiTool_input/annotated data/processed/CEMiTool_input/gene_conversion data/processed/CEMiTool_input/filtered tmp/modules results/CEMiTool_joined/enrichment/; do
 	echo "Creating $cdir ..."
 	if [ -d $cdir ]; then
 		echo "$cdir already exists !"
@@ -10,6 +10,28 @@ for cdir in results/CEMiTool data/processed/CEMiTool_input/annotated data/proces
 	fi
 done
 
-parallel -j 10 "src/microarrayAnalysis/genefilter.R --expr-file {} --output {/} --outdir data/processed/CEMiTool_input --method V --pvalue 0.3 > log/genefilter_{/.}.txt" ::: data/processed/collapsed/*.tsv
-parallel "src/annotateCEMiTool_input.R {} Symbol" ::: data/processed/CEMiTool_input/GSE*.tsv
-parallel -j 10 "src/microarrayAnalysis/CEMiTool.R  {} -o results/CEMiTool/{/.}_CEMiTool -s config/pathways/BTM.gmt -t config/sample_annotation/{/.}.tsv --gene-column Symbol --samples-column Sample_geo_accession -c spearman -n 1000 > log/CEMiTool_{/.}.txt 2> log/CEMiTool_{/.}.txt" :::  data/processed/CEMiTool_input/annotated/*.tsv
+# filter using Gustavo's script
+parallel -j 10 "src/microarrayAnalysis/genefilter.R --expr-file {} --output {/} --outdir data/processed/CEMiTool_input/filtered --method V --pvalue 0.3 > log/genefilter_{/.}.txt" ::: data/processed/collapsed/*.tsv
+
+# prepare input for CEMiTool
+parallel "src/annotateCEMiTool_input.R {} data/processed/CEMiTool_input/ Symbol" ::: data/processed/CEMiTool_input/filtered/GSE*.tsv
+
+#run CEMiTool
+parallel -j 1 "src/microarrayAnalysis/CEMiTool.R  {} -o results/CEMiTool/{/.}_CEMiTool -s config/pathways/BTM.gmt -t config/sample_annotation/{/.}.tsv --gene-column Symbol --samples-column Sample_geo_accession -c spearman -n 1000 > log/CEMiTool_{/.}.txt 2> log/CEMiTool_{/.}.txt" :::  data/processed/CEMiTool_input/annotated/*.tsv
+
+echo  "Joining cemitool results ..."
+src/microarrayAnalysis/integrateCemitResult.R --output results/CEMiTool_joined.tsv results/CEMiTool/*_GenesInModules.txt
+
+echo "Getting connected components in joined cemitool results ..."
+src/microarrayAnalysis/get_modules.py --input results/CEMiTool_joined.tsv --from-col G1 --to-col G2 --filter-col sumOfPairs --filter-val 3 --output results/CEMiTool_joined_modules.txt
+
+echo "Putting each module in one file ..."
+parallel 'echo {} | tr "\t" "\n" > tmp/modules/mod{#}.txt' :::: results/CEMiTool_joined_modules.txt
+
+cho "Getting all genesets in enrichr ..."
+genesets=$(cat config/libraries_enrichr.txt | sed "s/^/--gs=/" | tr "\n" " ")
+
+echo "Running enrichr ..."
+parallel -j 20 "src/microarrayAnalysis/enrichr.py --input {} --output results/CEMiTool_joined/enrichment/{/.}.tsv $genesets" ::: tmp/modules/*.txt
+
+echo "Done."
