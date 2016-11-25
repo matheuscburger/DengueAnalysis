1ddc7f7d99aa473167758b1b58ea2f4a588b3ef0
matheuscburger <matheus.cburger@gmail.com>
2016-11-25 17:41:51 -0200

    convert EnsemblsIDs em Symbols usando biomart
    Former-commit-id: 12643a03a814518f9df4570f127b48001bf2be2b

---
+++ src/microarrayAnalysis/ensembl2symbol.R
@@ -0,0 +1,59 @@
+#!/usr/bin/env Rscript
+
+"Ensembl ID To Gene Symbols
+
+Usage: ensembl2symbol.R --input=<file> --output=<file> --ensembl-col=<value> [--attributes=<value>...] 
+
+Options:
+  -h --help                  show this help message
+  --version                  show program version
+  --input=<file>             input file name
+  --output=<file>            output file name
+  --ensembl-col=<value>      column containing ensembl ids
+  --attributes=<value>       aditional attributes [default: external_gene_name]
+
+Authors:
+  Matheus Carvalho Burger - burger at usp.br
+
+More information:
+  www.csbiology.com
+  Computational Systems Biology Laboratory
+  University of Sao Paulo, Brazil
+" -> doc
+
+
+suppressMessages(library("readr"))
+suppressMessages(library("dplyr"))
+suppressMessages(library("tidyr"))
+suppressMessages(library("stringr"))
+suppressMessages(library("biomaRt"))
+
+if (!interactive() && !exists('SOURCE')) {
+	# Get and check arguments.
+	suppressMessages(library(docopt))
+	arg <- docopt(doc, version="0.0.1\n", strict=T)
+	arg <- arg[!sapply(arg, is.null)][-(1:2)]  # filter missing, 'help' and 'version'
+	clean <- function(s) gsub('-', '_', gsub('^-+', '', tolower(s)))
+	names(arg) <- clean(names(arg))
+
+
+	## RUN
+	#arg <- list()
+	#arg$input <- "data/processed/filtered/GSE13052.tsv"
+	#arg$ensembl_col <- "Symbol"
+	#arg$attributes <- c("external_gene_name")
+	#arg$output <- "comsymbols.tsv"
+
+	in_df <- read_tsv(arg$input)
+	ensgs <- sort(unique(in_df[[arg$ensembl_col]]))
+
+	ensembl <- useMart("ENSEMBL_MART_ENSEMBL", dataset="hsapiens_gene_ensembl", host="ensembl.org")
+	gsymbols <- getBM(attributes=c("ensembl_gene_id", "hgnc_symbol", arg$attributes), filters="ensembl_gene_id",
+					  values=ensgs, mart=ensembl)
+
+	by_vec <- arg$ensembl_col
+	names(by_vec) <- "ensembl_gene_id"
+
+	in_df %>% right_join(gsymbols, ., by=by_vec) %>%
+		write_tsv(arg$output)
+}
