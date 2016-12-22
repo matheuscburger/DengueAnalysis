2467f9b592cc58deac2ebac423413c90770eea20
matheuscburger <matheus.cburger@gmail.com>
2016-12-22 12:53:45 -0200

    run gsea
    Former-commit-id: bca140449a86ca70d61d0c46da27deee6748c165

--- src/gsea.sh
+++ src/gsea.sh
@@ -1,4 +1,4 @@
-for cdir in tmp/fgsea/annotated tmp/fgsea/colapsed results/fgsea; do
+for cdir in tmp/fgsea/Log2FC tmp/fgsea/annotated tmp/fgsea/colapsed tmp/fgsea/scaled results/fgsea; do
     echo "Creating $cdir ..."
     if [ -d $cdir ]; then
         echo "$cdir already exists !"
@@ -7,11 +7,35 @@ for cdir in tmp/fgsea/annotated tmp/fgsea/colapsed results/fgsea; do
         echo "$cdir created !"
     fi
 done
+for database in config/pathways/*.gmt; do
+	db=$(basename $database .gmt)
+	for cdir in results/fgsea/Log2FC/$db results/fgsea/MeanZScore/$db figures/CEMiTool_joined_corrplot/Log2FC/$db figures/CEMiTool_joined_corrplot/MeanZScore/$db; do
+		echo "Creating $cdir ..."
+		if [ -d $cdir ]; then
+			echo "$cdir already exists !"
+		else
+			mkdir -p $cdir
+			echo "$cdir created !"
+		fi
+	done
+done
+
+parallel -j 10 "src/microarrayAnalysis/comparisons2gseainput.R --input {} --output tmp/fgsea/Log2FC/{/} "  ::: results/DEG/*.tsv
 
 parallel -j 10 "src/microarrayAnalysis/ensembl2symbol.R --input {} --output tmp/fgsea/annotated/{/} --ensembl-col Symbol"  ::: data/processed/filtered/*.tsv
 
 parallel -j 10 "src/microarrayAnalysis/collapse.R {} tmp/fgsea/colapsed/{/} --by-col external_gene_name --method maxmean --annotation-cols  hgnc_symbol --annotation-cols ensembl_gene_id --annotation-cols ProbeName" ::: tmp/fgsea/annotated/*.tsv
 
+parallel -j 10 "src/microarrayAnalysis/scale.R --expr-file {} --output tmp/fgsea/scaled/{/} --annotation-cols external_gene_name" ::: tmp/fgsea/colapsed/*.tsv
+
+parallel -j 10 "src/microarrayAnalysis/group_mean.R --expr-file {} --group-file config/sample_annotation/{/} --output tmp/fgsea/meanzscore/{/} --group-col Stage --samplename-col Sample_geo_accession" ::: tmp/fgsea/scaled/*.tsv
+
 for database in config/pathways/*.gmt; do
-    parallel --progress -j 10 "mkdir -p results/fgsea/{/.}/$database; src/microarrayAnalysis/fgsea.R --expression {} --es results/fgsea/{/.}/$database/es.tsv --nes results/fgsea/{/.}/$database/nes.tsv --pval results/fgsea/{/.}/$database/pval.tsv --gmt config/pathways/ReactomePathways.gmt --sample-annotation config/sample_annotation/{/} --symbols external_gene_name --sample-name-col Sample_geo_accession --class-col Class" ::: tmp/fgsea/colapsed/*.tsv
+    db=$(basename $database .gmt)
+    # parallel --progress -j 10 "mkdir -p results/fgsea/{/.}/$db; src/microarrayAnalysis/fgsea.R --expression {} --es results/fgsea/{/.}/$db/es.tsv --nes results/fgsea/{/.}/$db/nes.tsv --pval results/fgsea/{/.}/$db/pval.tsv --gmt config/pathways/ReactomePathways.gmt --sample-annotation config/sample_annotation/{/} --symbols external_gene_name --sample-name-col Sample_geo_accession --class-col Class" ::: tmp/fgsea/colapsed/*.tsv
+    parallel --progress -j 10 "src/microarrayAnalysis/fgsea.R --input {} --output results/fgsea/Log2FC/$db/{/} --gmt $database --symbols hgnc_symbol" ::: tmp/fgsea/Log2FC/*.tsv
+    parallel --progress -j 10 "src/microarrayAnalysis/fgsea.R --input {} --output results/fgsea/MeanZScore/$db/{/} --gmt $database --symbols external_gene_name" ::: tmp/fgsea/meanzscore/*.tsv
+
+    parallel --progress -j 10 "src/microarrayAnalysis/corrplot_fgsea.R {} figures/CEMiTool_joined_corrplot/Log2FC/$db/{/.}.pdf" ::: results/fgsea/Log2FC/$db/*.tsv
+    parallel --progress -j 10 "src/microarrayAnalysis/corrplot_fgsea.R {} figures/CEMiTool_joined_corrplot/MeanZScore/$db/{/.}.pdf" ::: results/fgsea/MeanZScore/$db/*.tsv
 done
