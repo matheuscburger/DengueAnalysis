0211f8a319e239682322cb12cd434fd54445817d
matheuscburger <matheus.cburger@gmail.com>
2017-04-12 18:39:06 -0300

    Included Reactome; Fix the path for tables
    Former-commit-id: 5e7ee551efa95c1bb14950a07ad755b6cba7d344

--- src/figures/enrichment_degs.sh
+++ src/figures/enrichment_degs.sh
@@ -14,5 +14,11 @@ echo "Enrichment plots for enrichr ..."
 #parallel -j 10 "src/microarrayAnalysis/enrichment_plots.R --input {} --output figures/enrichment_degs/enrichr_{/.}.pdf" ::: results/joined_degs/enrichment/enrichr/*.tsv
 
 echo "Enrichment plots for BTMs ..."
-parallel -j 10 "src/microarrayAnalysis/enrichment_plots.R --input {} --output figures/enrichment_degs/BTM_{/.}.pdf --term-col ID --pv-col p.adjust --title BTM --rgb-color '#005417' " ::: results/joined_degs/enrichment/do_ora/*Down*regulated.tsv
-parallel -j 10 "src/microarrayAnalysis/enrichment_plots.R --input {} --output figures/enrichment_degs/BTM_{/.}.pdf --term-col ID --pv-col p.adjust --title BTM --rgb-color '#7D0002' " ::: results/joined_degs/enrichment/do_ora/*Up*regulated.tsv
+parallel -j 10 "src/microarrayAnalysis/enrichment_plots.R --input {} --output figures/enrichment_degs/BTM_{/.}.pdf --term-col ID --pv-col p.adjust --title BTM --rgb-color '#005417' " ::: results/enrichment/do_ora/BTM/*Down*regulated.tsv
+parallel -j 10 "src/microarrayAnalysis/enrichment_plots.R --input {} --output figures/enrichment_degs/BTM_{/.}.pdf --term-col ID --pv-col p.adjust --title BTM --rgb-color '#7D0002' " ::: results/enrichment/do_ora/BTM/*Up*regulated.tsv
+
+
+echo "Enrichment plots for Reactome ..."
+parallel -j 10 "src/microarrayAnalysis/enrichment_plots.R --input {} --output figures/enrichment_degs/Reactome_{/.}.pdf --term-col ID --pv-col p.adjust --title Reactome --rgb-color '#005417' " ::: results/enrichment/do_ora/ReactomePathways/*Down*regulated.tsv
+parallel -j 10 "src/microarrayAnalysis/enrichment_plots.R --input {} --output figures/enrichment_degs/Reactome_{/.}.pdf --term-col ID --pv-col p.adjust --title Reactome --rgb-color '#7D0002' " ::: results/enrichment/do_ora/ReactomePathways/*Up*regulated.tsv
+
