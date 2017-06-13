7dc3ec67ea1076b0bd4747a0fa379f115c45b735
matheuscburger <matheus.cburger@gmail.com>
2017-06-13 20:43:50 -0300

    Uma modificacao para tentar novamente caso a internet falhe ao baixar um estudo

--- src/getData.sh
+++ src/getData.sh
@@ -10,9 +10,15 @@ for cdir in log data/raw_data data/geo_raw/author data/geo_raw/supplemental_data
 	fi
 done
 
+
+# $? eh o status do ultimo comando
 GetGEO() {
 	echo "Downloading $1 ..."
 	src/microarrayAnalysis/get_geo.R $1 $2 --author-dir=data/geo_raw/author/ --sup-dir=data/geo_raw/supplemental_data/ --sample-annot-dir=data/geo_raw/sample_annotation/ --probe-annot-dir=data/geo_raw/probe_annotation/ --raw-dir=data/geo_raw/raw_data/ 1>&2 2> log/getdata/get_geo_$1.log 
+	while [ $? -ne 0 ]; do
+		echo "Trying again ..."
+		src/microarrayAnalysis/get_geo.R $1 $2 --author-dir=data/geo_raw/author/ --sup-dir=data/geo_raw/supplemental_data/ --sample-annot-dir=data/geo_raw/sample_annotation/ --probe-annot-dir=data/geo_raw/probe_annotation/ --raw-dir=data/geo_raw/raw_data/ 1>&2 2> log/getdata/get_geo_$1.log 
+	done	
 }
 
 
