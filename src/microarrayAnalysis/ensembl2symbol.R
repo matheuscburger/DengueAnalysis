b2b2865c2ab76c78917697dfe0f707dd3ebd43d1
matheuscburger <matheus.cburger@gmail.com>
2017-06-17 19:58:20 -0300

    New option; give a list instead of a table

--- src/microarrayAnalysis/ensembl2symbol.R
+++ src/microarrayAnalysis/ensembl2symbol.R
@@ -2,15 +2,16 @@
 
 "Ensembl ID To Gene Symbols
 
-Usage: ensembl2symbol.R --input=<file> --output=<file> --ensembl-col=<value> [--attributes=<value>...] 
+Usage: ensembl2symbol.R --input=<file> --output=<file> [--ensembl-col=<value> --is-list --attributes=<value>...] 
 
 Options:
   -h --help                  show this help message
   --version                  show program version
-  --input=<file>             input file name
+  --input=<file>             input file name (table or list)
   --output=<file>            output file name
   --ensembl-col=<value>      column containing ensembl ids
   --attributes=<value>       aditional attributes [default: external_gene_name]
+  --is-list                   input file is a list
 
 Authors:
   Matheus Carvalho Burger - burger at usp.br
@@ -29,31 +30,37 @@ suppressMessages(library("stringr"))
 suppressMessages(library("biomaRt"))
 
 if (!interactive() && !exists('SOURCE')) {
-	# Get and check arguments.
-	suppressMessages(library(docopt))
-	arg <- docopt(doc, version="0.0.1\n", strict=T)
-	arg <- arg[!sapply(arg, is.null)][-(1:2)]  # filter missing, 'help' and 'version'
-	clean <- function(s) gsub('-', '_', gsub('^-+', '', tolower(s)))
-	names(arg) <- clean(names(arg))
+    # Get and check arguments.
+    suppressMessages(library(docopt))
+    arg <- docopt(doc, version="0.0.1\n", strict=T)
+    arg <- arg[!sapply(arg, is.null)][-(1:2)]  # filter missing, 'help' and 'version'
+    clean <- function(s) gsub('-', '_', gsub('^-+', '', tolower(s)))
+    names(arg) <- clean(names(arg))
 
 
-	## RUN
-	#arg <- list()
-	#arg$input <- "data/processed/filtered/GSE13052.tsv"
-	#arg$ensembl_col <- "Symbol"
-	#arg$attributes <- c("external_gene_name")
-	#arg$output <- "comsymbols.tsv"
+    ## RUN
+    #arg <- list()
+    #arg$input <- "data/processed/filtered/GSE13052.tsv"
+    #arg$ensembl_col <- "Symbol"
+    #arg$attributes <- c("external_gene_name")
+    #arg$output <- "comsymbols.tsv"
 
-	in_df <- read_tsv(arg$input)
-	ensgs <- sort(unique(in_df[[arg$ensembl_col]]))
+    if("is_list" %in% names(arg)){
+        ensgs <- sort(unique(readLines(arg$input)))
+    } else {
+        in_df <- read_tsv(arg$input)
+        ensgs <- sort(unique(in_df[[arg$ensembl_col]]))
+    }
 
-	ensembl <- useMart("ENSEMBL_MART_ENSEMBL", dataset="hsapiens_gene_ensembl", host="ensembl.org")
-	gsymbols <- getBM(attributes=c("ensembl_gene_id", "hgnc_symbol", arg$attributes), filters="ensembl_gene_id",
-					  values=ensgs, mart=ensembl)
+    ensembl <- useMart("ENSEMBL_MART_ENSEMBL", dataset="hsapiens_gene_ensembl", host="ensembl.org")
+    gsymbols <- getBM(attributes=c("ensembl_gene_id", "hgnc_symbol", arg$attributes), filters="ensembl_gene_id",
+                      values=ensgs, mart=ensembl)
 
-	by_vec <- arg$ensembl_col
-	names(by_vec) <- "ensembl_gene_id"
 
-	in_df %>% right_join(gsymbols, ., by=by_vec) %>%
-		write_tsv(arg$output)
+    if(!arg[["is_list"]]){
+        by_vec <- arg$ensembl_col
+        names(by_vec) <- "ensembl_gene_id"
+        gsymbols <- in_df %>% right_join(gsymbols, ., by=by_vec) 
+    }
+    gsymbols %>% write_tsv(arg$output)
 }
